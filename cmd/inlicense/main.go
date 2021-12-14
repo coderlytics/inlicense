b69211ef37e03b73f0143d7f8cc70df87725dcbb
msieber88 <msieber88@gmail.com>
2021-12-14 16:03:21 +0100

    switch to rsa

--- cmd/inlicense/main.go
+++ cmd/inlicense/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"coderlytics.at/inlicense/internal"
@@ -30,29 +32,42 @@ func main() {
 
 // generateKeyPair generates the private and public key pair for creating and validating a license
 func generateKeyPair(cmd *cobra.Command, args []string) {
-	privateKey, err := internal.NewPrivateKey()
+	priv, pub, err := internal.GenerateBase64Keys()
 
 	if err != nil {
-		println("Error generating private key")
-		println(err)
+		println("Error generating key pair")
+		println(err.Error())
 		os.Exit(1)
 	}
 
-	b64Key, err := privateKey.ToBase64String()
-
-	if err != nil {
-		println("Error converting private key to base64 string")
-		println(err)
-		os.Exit(1)
-	}
-	println(fmt.Sprintf("Private key (keep in a secure place): %s", b64Key))
-
-	publicKey := privateKey.GetPublicKey()
-	b64pKey := publicKey.ToBase64String()
-	println(fmt.Sprintf("Public key (put into your code to verify the license): %s", b64pKey))
+	println(fmt.Sprintf("Private key (keep in a secure place): %s", priv))
+	println(fmt.Sprintf("Public key (put into your code to verify the license): %s", pub))
 }
 
 // generateLicense generates the license file using the private key and the license configuration file
 func generateLicense(cmd *cobra.Command, args []string) {
-	// TODO implement
+	cfgFile := args[0]
+	privKey := args[1]
+
+	if _, err := os.Stat(cfgFile); errors.Is(err, os.ErrNotExist) {
+		println(fmt.Sprintf("Configuration file %s does not exist", cfgFile))
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadFile(cfgFile)
+
+	if err != nil {
+		println(fmt.Sprintf("Error reading file %s", cfgFile))
+		println(err.Error())
+	}
+
+	lic, err := internal.GenerateLicense(data, privKey)
+
+	if err != nil {
+		println("Unable to create license key")
+		println(err.Error())
+		os.Exit(1)
+	}
+
+	println(lic)
 }
--- internal/keys.go
+++ internal/keys.go
@@ -2,55 +2,52 @@ package internal
 
 import (
 	"bytes"
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/rsa"
 	"encoding/base64"
 	"encoding/gob"
 )
 
-// PrivateKey is the master key to create a licenses.
-// Private key needs to be put in a safe location
-type PrivateKey ecdsa.PrivateKey
-
-// PublicKey is used to check the validity of the licenses. You can share it
-// freely.
-type PublicKey ecdsa.PublicKey
-
-// NewPrivateKey generates a new private key
-func NewPrivateKey() (*PrivateKey, error) {
-	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+// GenerateKeys generates a private and public key pair
+func GenerateKeys() (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privkey, err := rsa.GenerateKey(rand.Reader, 368)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
+	}
+	return privkey, &privkey.PublicKey, nil
+}
+
+// GenerateBase64Keys generates a private and public key pair and encodes them to base64 strings
+func GenerateBase64Keys() (string, string, error) {
+	priv, pub, err := GenerateKeys()
+
+	if err != nil {
+		return "", "", err
 	}
 
-	privateKey := PrivateKey(*key)
-	return &privateKey, nil
+	privB64, err := toB64String(priv)
+
+	if err != nil {
+		return "", "", err
+	}
+
+	pubB64, err := toB64String(pub)
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return privB64, pubB64, nil
 }
 
-// ToBytes transforms the private key to a  []byte.
-func (k PrivateKey) ToBytes() ([]byte, error) {
-	ek := k.toEcdsa()
-	c := elliptic.Marshal(
-		ek.PublicKey.Curve,
-		ek.PublicKey.X,
-		ek.PublicKey.Y)
-
-	return toBytes(c)
-}
-
-// ToBase64String transforms the private key to a base64 string.
-func (k PrivateKey) ToBase64String() (string, error) {
-	b, err := k.ToBytes()
+// toB64String converts an object to a base64 encoded string
+func toB64String(obj interface{}) (string, error) {
+	b, err := toBytes(obj)
 	if err != nil {
 		return "", err
 	}
-	return base64.StdEncoding.EncodeToString(b), nil
-}
 
-func (k *PrivateKey) toEcdsa() *ecdsa.PrivateKey {
-	r := ecdsa.PrivateKey(*k)
-	return &r
+	return base64.StdEncoding.EncodeToString(b), nil
 }
 
 func toBytes(obj interface{}) ([]byte, error) {
@@ -62,29 +59,4 @@ func toBytes(obj interface{}) ([]byte, error) {
 	}
 
 	return buffBin.Bytes(), nil
-}
-
-// GetPublicKey returns the PublicKey associated with the private key.
-func (k PrivateKey) GetPublicKey() *PublicKey {
-	pk := PublicKey(k.PublicKey)
-	return &pk
-}
-
-// ToBase64String transforms the public key to a base64 string.
-func (k PublicKey) ToBase64String() string {
-	return base64.StdEncoding.EncodeToString(
-		k.ToBytes(),
-	)
-}
-
-// ToBytes transforms the public key to a []byte.
-func (k PublicKey) ToBytes() []byte {
-	// return toBytes(k)
-	ek := k.toEcdsa()
-	return elliptic.Marshal(ek.Curve, ek.X, ek.Y)
-}
-
-func (k *PublicKey) toEcdsa() *ecdsa.PublicKey {
-	r := ecdsa.PublicKey(*k)
-	return &r
 }
